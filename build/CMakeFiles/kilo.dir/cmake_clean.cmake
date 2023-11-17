d8430fc49555f0d1a912955d5d47045efab33518
Amarantos475 <2529158916@qq.com>
2023-11-17 14:36:01 +0800

    1.完成进入原始模式模块 2.完成原始输入和输出模块 3.完成文本查看器模块 4.完成文本编辑器模块 5.完成搜索模块

---
+++ build/CMakeFiles/kilo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kilo.dir/kilo.c.o"
+  "CMakeFiles/kilo.dir/kilo.c.o.d"
+  "kilo"
+  "kilo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/kilo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
