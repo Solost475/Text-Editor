2922c9f4ffe4aaa5a3bdd60fa5963459dcacbd3f
solost475 <solost475@qq.com>
2025-03-26 17:02:40 +0800

    Create Makefile

---
+++ 1.cpp
@@ -0,0 +1,50 @@
+#include <map>
+#include <set>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <bitset>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+
+#define x first
+#define y second
+#define endl '\n'
+#define IOS                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fp(i, a, b) for (int i = a, i##_ = b; i <= i##_; ++i)
+#define fd(i, a, b) for (int i = a, i##_ = b; i >= i##_; --i)
+
+using namespace std;
+
+using ll = long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using ull = unsigned long long;
+
+const int INF = 0x3f3f3f3f, mod = 1000000007;
+const int N = 2e5 + 10;
+
+void solve()
+{
+    /* code */
+}
+int main()
+{
+    // IOS;
+    int _ = 1;
+    // cin >> _;
+    while (_--) {
+        solve();
+    }
+    return 0;
+}
